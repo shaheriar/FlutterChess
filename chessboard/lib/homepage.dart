cc81c0b0f2126ccb68df1f82252a27d480d8e7a9
shaheriar <49822364+shaheriar@users.noreply.github.com>
2021-12-13 04:47:41 -0600

    Update homepage.dart

--- chessboard/lib/homepage.dart
+++ chessboard/lib/homepage.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   title() {
     return Text(
-      'Flython Chess',
+      'Flutter Chess',
       style: TextStyle(
         color: Colors.white,
         fontSize: 80,
